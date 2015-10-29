6af8549a7983286539e5599f8c8c900f8bc243cd
XGuevaraX <gusyjos0701@gmail.com>
2015-10-29 15:19:30 -0600

    Facebook

---
+++ DataBase/app/src/main/java/com/xguevarax/database/MainActivity.java
@@ -0,0 +1,52 @@
+package com.xguevarax.database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Peticion(View view)
+    {
+        final RequestQueue queue = Volley.newRequestQueue(this);//Debemos pasar el contexto de la actividad(Activity)
+
+        //creamos una instancia de StringRequest
+        //Recibe tres parametros
+        //1)Metodo[Get-POST]
+        //2)URL[Server]Se encuentra en la interface Value
+        //3)Respuesta[]
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://jsonplaceholder.typicode.com/posts", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {//Si la respuesta es positiva,si todo salio OK
+               Log.e("Respuesta", response);
+            }
+        }, new Response.ErrorListener() {//En el caso de que ocurra un error X
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error: ", error.getMessage() + "");//Se mostrara el error producido
+            }
+        });
+        queue.add(stringRequest);
+
+    }
+}
